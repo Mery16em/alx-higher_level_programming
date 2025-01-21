a87ffe91b8b3a8b9e8d16017ababa95e0c7a4653
Meryem <ml.meryem.16@gmail.com>
2025-01-21 04:18:55 -0800

    update code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays "my index page" if the response is exactly that
 curl -s "$1" | grep -q "my index page" && echo "GET / => \"my index page\""
-
